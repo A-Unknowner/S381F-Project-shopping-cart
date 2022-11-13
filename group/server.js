44ae677dd2c1522595ef20f7ae87f9fe32aea37c
A-Unknowner <116628628+A-Unknowner@users.noreply.github.com>
2022-11-13 17:25:49 +0800

    Update server.js

--- group/server.js
+++ group/server.js
@@ -96,9 +96,7 @@ app.get('/home', (req, res) => {
 
             findDocument(db, {}, "item", (docs) => {
                 client.close();
-                for (var i of docs){
-                    res.status(200).render("home", { itemList : i });
-                }
+
                 // for (var i of docs){
                 //     res.status(200).render("home", { itemList : i });
                 // }
